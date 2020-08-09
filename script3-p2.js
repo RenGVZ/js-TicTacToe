76110d0d7eb85774f13d62fec0a506c815eccde5
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-09 15:12:13 +0900

    finished last script-3

--- script3-p2.js
+++ script3-p2.js
@@ -1,8 +1,8 @@
 // create 2 variables, xClass and oClass and set them to x and o values
-const xClass = 'x';
-const oClass = 'o';
+const xClass = 'xClass';
+const oClass = 'oClass';
 // create an array of all of the winning combos
-const winningArray = [
+let winningArray = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -17,7 +17,7 @@ const cellElements = document.querySelectorAll('.grid-item');
 // grab the grid and name it board
 const board = document.getElementById('grid');
 // create a winningMessageTextElement
-let winningMessageTextElement = document.getElementById('game-status');
+const winningMessageTextElement = document.getElementById('game-status');
 // grab the restart button
 const reset = document.getElementById('reset');
 // make a variable called circleTurn, don't set it yet. If it's true it will be O's turn and if not it's X's turn
@@ -52,43 +52,47 @@ function startGame() {
 // make the handleClick function
 function handleClick(e) {
   // make cell var which will be the event target
-  let cell = e.target
+  const cell = e.target
   // make currentClass which is a ternary operator based on the state of circkleTurn, xClass or oClass
-  let currentClass = circleTurn ? xClass : oClass;
+  const currentClass = circleTurn ? oClass : xClass;
   // call placeMark function that gets the cell and the currentClass. Define function outside
   placeMark(cell, currentClass);
   // add an if statement to check checkWin, passing in the currentClass and create checkWin function outside
   if(checkWin(currentClass)) {
       // call endGame function with a false parameter
-      
+      endGame(false)
       // else if check for draw, calling the isDraw function
-
+   } else if(isDraw()) {
       // the result of the statement being true is that endGame is called with a true value
-      
-
-      // else call the switchTurns function
-      
-      // and call the setBoardHoverClass function
-  } 
-}
+      endGame(true);
+      } else {
+        // else call the switchTurns function
+        switchTurns();
+        // and call the setBoardHoverClass function
+        setBoardHoverClass();
+      }
+  }
 
 // make endGame function with draw passed in
-
+function endGame(draw) {
   // if there's a draw
-  
+  if(draw) {
     // set the text as 'draw' for the winningMessageTextElement
-    
-
+    winningMessageTextElement.innerText = 'Draw'
+  } else {
     // else make a ternerary operator displaying the name of the winning team + 'win' inside the winningTextMessage element
-    
-
+    winningMessageTextElement.innerText = `${circleTurn? "O's" : "X's"} win!`
+  }
+}
 
 // make the isDraw function
-
+function isDraw() {
   // check if every element of the cell has a class, first you have to destructure the cellElements into an array
-  
-    // then return the cells which contain the classList of oClass or xClass
-    
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(xClass) ||
+    cell.classList.contains(oClass);
+  })
+}
 
 
 // make a placeMark function that takes the cell and the currentClass
@@ -98,9 +102,10 @@ function placeMark(cell, currentClass) {
 }
 
 // make switchTurns function
-
+function switchTurns() {
   // assign circleTurn as the opposite of itself
-  
+  circleTurn = !circleTurn;
+}
 
 
 // make the setBoardHoverClass function and change the classList of board
@@ -126,7 +131,7 @@ function checkWin(currentClass) {
     // call an every method on the winningArray's elements
     return combo.every(i => {
       // check if the value of the cellElement at the index of this iterated element contains the current class's classList
-      return cellElements[i].classList.contains(currentClass)
+      return cellElements[i].classList.contains(currentClass);
     })
   })
 }
